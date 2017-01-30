e4b1f11247968e56764630a4320e2be6742585e4
Spencer Lievens <spencerlievens@users.noreply.github.com>
2017-01-30 11:22:54 +0100

    Update test_jsonrpc.py

--- test/integration/test_jsonrpc.py
+++ test/integration/test_jsonrpc.py
@@ -14,14 +14,14 @@ from darksilk_config import DarkSilkConfig
 
 def test_darksilkd():
     config_text = DarkSilkConfig.slurp_config_file(config.darksilk_conf)
-    creds = DarkSilkConfig.get_rpc_creds(config_text, 'testnet')
+    creds = DarkSilkConfig.get_rpc_creds(config_text, 'mainnet')
 
     darksilkd = DarkSilkDaemon(**creds)
     assert darksilkd.rpc_command is not None
 
     assert hasattr(darksilkd, 'rpc_connection')
 
-    # DarkSilk testnet block 0 hash == 0006dc5ab20561a3e49e112402beb5f451d7e82ce67f394c54480099dc241d88
+    # DarkSilk testnet block 0 hash == 0000b0f60bd743b8b5ed3e62a1fbad5d2c073d2b9cb3baa108872dbe1315e48e
     # test commands without arguments
     info = darksilkd.rpc_command('getinfo')
 
@@ -38,7 +38,7 @@ def test_darksilkd():
     ]
     for key in info_keys:
         assert key in info
-    assert info['testnet'] is True
+    assert info['mainnet'] is True
 
     # test commands with args
-    assert darksilkd.rpc_command('getblockhash', 0) == u'0006dc5ab20561a3e49e112402beb5f451d7e82ce67f394c54480099dc241d88'
+    assert darksilkd.rpc_command('getblockhash', 0) == u'0000b0f60bd743b8b5ed3e62a1fbad5d2c073d2b9cb3baa108872dbe1315e48e'
